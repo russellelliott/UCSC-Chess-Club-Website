6c90e3047120d8e9985983f9c9019f3e251ca506
Russell Elliott <russell@quadsquad.net>
2023-01-09 16:14:14 -0800

    Adedd navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {Route, NavLink, BrowserRouter, Routes} from "react-router-dom";
+
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <h1>UCSC Chess Club</h1>
+      <ul className="navigation">
+        <li><NavLink exact="true" to="/">Home</NavLink></li>
+        <li><NavLink to="/tech">Tech</NavLink></li>
+        <li><NavLink to="/help">Help</NavLink></li>
+      </ul>
+      <div>
+        <Routes>
+          <Route exact="true" path="/" element={<Home/>}/>
+          <Route path="/tech" element={<Home/>}/>
+          <Route path="/help" element={<Home/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
